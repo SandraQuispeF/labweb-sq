8c2f98b264241f4cde37d8106f5b1e57a1f22dbb
SandraQuispeF <sandra.quispe.f@tecsup.edu.pe>
2022-11-06 19:41:33 -0500

    lab11

--- lab11/src/app/app.component.ts
+++ lab11/src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SelectornumericoComponent } from './selectornumerico/selectornumerico.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'lab11';
+
+  //Dado
+  valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado: string = "";
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1 == this.valor2 && this.valor1 == this.valor3)
+      this.resultado = 'Ganó';
+    else
+      this.resultado = 'Perdió';
+  }
+
+  //Cronometro
+  mensaje = '';
+
+  actualizar(t: number) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }
+
+  //Selector Numerico
+  @ViewChild('selector1') selector1!: SelectornumericoComponent;
+  @ViewChild('selector2') selector2!: SelectornumericoComponent;
+
+  fijarSelector1(valor: number) {
+    this.selector1.fijar(valor);
+  }
 }
--- lab11/src/app/app.module.ts
+++ lab11/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SelectornumericoComponent } from './selectornumerico/selectornumerico.component';
+import { CronometroComponent } from './cronometro/cronometro.component';
+import { DadoComponent } from './dado/dado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectornumericoComponent,
+    CronometroComponent,
+    DadoComponent
   ],
   imports: [
     BrowserModule,
